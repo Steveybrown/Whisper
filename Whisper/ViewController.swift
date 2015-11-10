02a3925e27eff26bcd3bccb9831153ca4628ceff
Stephen Brown <steveybrown@gmail.com>
2015-11-10 18:38:40 +0000

    Initial Commit

---
+++ Whisper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whisper
+//
+//  Created by Stephen Brown on 10/11/2015.
+//  Copyright © 2015 Stephen Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
